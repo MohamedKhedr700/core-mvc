c5a26b509ce6090bbd9fe12f10ebae8c1e0a97e2
mohamedkhedr700 <M.khedr@rowaad.net>
2024-01-28 23:08:49 +0200

    Add abstract core actions

---
+++ app/Actions/Core/UpdateAction.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Actions\Core;
+
+use App\Models\User;
+use Raid\Core\Action\Actions\Contracts\Crud\UpdateActionInterface;
+use Raid\Core\Action\Actions\Crud\UpdateAction as RaidUpdateAction;
+
+class UpdateAction extends RaidUpdateAction implements UpdateActionInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function handle(string|object $id, array $data): bool
+    {
+        return $id->update($data);
+    }
+}
